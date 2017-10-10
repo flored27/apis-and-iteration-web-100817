e07340a22d2a4b503ac8bd73186469895b4a4338
flored27 <dflores@fandm.edu>
2017-10-10 15:52:54 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,23 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
+def find_character(character)
+
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  character_hash["results"].find{|x| x["name"].downcase == character
+end
+
+
+
+def get_character_movies_from_api(character)
+  #make the web request
+  films = find_character(character)["film"]
+films.map{|film| JSON.parse(RestClient.get(film)) }
+
+
+ end
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,9 +28,11 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
-end
+
 
 def parse_character_movies(films_hash)
+
+  
   # some iteration magic and puts out the movies in a nice list
 end
 
